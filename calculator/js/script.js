9b0a2cdd283e58b9c971d8d7ef0bceb0cce15927
NSVenkatesh <85620459+NSVenkatesh@users.noreply.github.com>
2021-06-16 22:30:36 +0530

    Add files via upload

--- calculator/js/script.js
+++ calculator/js/script.js
@@ -1,11 +1,49 @@
-function input(val){
-    document.getElementById('result').value+=val;
+// function input(val){
+//     document.getElementById('result').value+=val;
+// }
+// function solve(){
+//     let x=document.getElementById('result').value;
+//     let y=eval(x);
+//     document.getElementById('result').value=y;
+// }
+// function clr(){
+//     document.getElementById('result').value="";
+// }
+function clr(){
+    $('#result').val("");
+}
+function del(){
+    a=$('#result').val();
+    b=a.slice(0,length-1);
+    $("#result").val(b);
+}
+function input(x){
+    var y=$("#result").val();
+    $("#result").val(y+x);
+}
+function operator(y){
+    x=$("#result").val();
+    $("#result").val("");
+    o=y;
 }
 function solve(){
-    let x=document.getElementById('result').value;
-    let y=eval(x);
-    document.getElementById('result').value=y;
-}
-function clr(){
-    document.getElementById('result').value="";
+    y=$("#result").val();
+    if(o=="+"){
+        $('#result').val(parseFloat(x)+parseFloat(y));
+    }
+    else if(o=="-"){
+        $('#result').val(parseFloat(x)-parseFloat(y));
+    }
+    else if(o=="*"){
+        $('#result').val(parseFloat(x)*parseFloat(y));
+    }
+    else if(o=="/"){
+        $('#result').val(parseFloat(x)/parseFloat(y));
+    }
+    else if(o=="%"){
+        $('#result').val(parseFloat(x)%parseFloat(y));
+    }
+    else if(o=="**"){
+        $('#result').val(parseFloat(x)**parseFloat(y));
+    }
 }
